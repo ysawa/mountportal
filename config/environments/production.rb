13c1f9ebf312ea88dd25539b0edc7288789de485
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-07-12 02:32:41 +0900

    modify: url in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Mountportal::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'mountportal.com', protocol: 'http' }
 end
