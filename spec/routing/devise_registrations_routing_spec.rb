e32da414564e16fa23d5f53f7f5b972607fac2f3
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-07-14 23:08:02 +0900

    add: tests for devise routings

--- spec/routing/devise_registrations_routing_spec.rb
+++ spec/routing/devise_registrations_routing_spec.rb
@@ -7,8 +7,16 @@ describe DeviseRegistrationsController do
       post("/users").should route_to("devise_registrations#create")
     end
 
+    it "routes to #edit" do
+      get("/users/edit").should route_to("devise_registrations#edit")
+    end
+
     it "routes to #new" do
       get("/users/sign_up").should route_to("devise_registrations#new")
+    end
+
+    it "routes to #update" do
+      put("/users").should route_to("devise_registrations#update")
     end
   end
 end
