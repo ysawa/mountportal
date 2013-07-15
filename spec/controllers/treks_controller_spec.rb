bfe3a84a595ff15fa75289006bea848adb3ec081
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-07-16 00:46:54 +0900

    fix: tests

--- spec/controllers/treks_controller_spec.rb
+++ spec/controllers/treks_controller_spec.rb
@@ -62,98 +62,140 @@ describe TreksController do
   end
 
   describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Trek" do
-        expect {
-          post :create, {trek: valid_attributes}
-        }.to change(Trek, :count).by(1)
-      end
-
-      it "assigns a newly created trek as @trek" do
+    context 'without singing in' do
+      it "redirects to sign in page" do
         post :create, {trek: valid_attributes}
-        assigns(:trek).should be_a(Trek)
-        assigns(:trek).should be_persisted
-      end
-
-      it "redirects to the created trek" do
-        post :create, {trek: valid_attributes}
-        response.should redirect_to(Trek.last)
+        response.should redirect_to new_user_session_path
       end
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved trek as @trek" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Trek.any_instance.stub(:save).and_return(false)
-        post :create, {trek: { "name" => "" }}
-        assigns(:trek).should be_a_new(Trek)
+    context 'with singing in' do
+      before :each do
+        user_sign_in
       end
 
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Trek.any_instance.stub(:save).and_return(false)
-        post :create, {trek: { "name" => "" }}
-        response.should render_template("new")
+      describe "with valid params" do
+        it "creates a new Trek" do
+          expect {
+            post :create, {trek: valid_attributes}
+          }.to change(Trek, :count).by(1)
+        end
+
+        it "assigns a newly created trek as @trek" do
+          post :create, {trek: valid_attributes}
+          assigns(:trek).should be_a(Trek)
+          assigns(:trek).should be_persisted
+        end
+
+        it "redirects to the created trek" do
+          post :create, {trek: valid_attributes}
+          response.should redirect_to(Trek.last)
+        end
       end
+
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved trek as @trek" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          Trek.any_instance.stub(:save).and_return(false)
+          post :create, {trek: { "name" => "" }}
+          assigns(:trek).should be_a_new(Trek)
+        end
+
+        it "re-renders the 'new' template" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          Trek.any_instance.stub(:save).and_return(false)
+          post :create, {trek: { "name" => "" }}
+          response.should render_template("new")
+        end
+      end
+
     end
   end
 
   describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested trek" do
-        trek = Trek.create! valid_attributes
-        # Assuming there are no other treks in the database, this
-        # specifies that the Trek created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Trek.any_instance.should_receive(:update).with({ "name" => "MyString" })
-        put :update, {id: trek.to_param, trek: { "name" => "MyString" }}
-      end
-
-      it "assigns the requested trek as @trek" do
+    context 'without singing in' do
+      it "redirects to sign in page" do
         trek = Trek.create! valid_attributes
         put :update, {id: trek.to_param, trek: valid_attributes}
-        assigns(:trek).should eq(trek)
-      end
-
-      it "redirects to the trek" do
-        trek = Trek.create! valid_attributes
-        put :update, {id: trek.to_param, trek: valid_attributes}
-        response.should redirect_to(trek)
+        response.should redirect_to new_user_session_path
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the trek as @trek" do
-        trek = Trek.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Trek.any_instance.stub(:save).and_return(false)
-        put :update, {id: trek.to_param, trek: { "name" => "" }}
-        assigns(:trek).should eq(trek)
+    context 'with singing in' do
+      before :each do
+        user_sign_in
       end
 
-      it "re-renders the 'edit' template" do
-        trek = Trek.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Trek.any_instance.stub(:save).and_return(false)
-        put :update, {id: trek.to_param, trek: { "name" => "" }}
-        response.should render_template("edit")
+      describe "with valid params" do
+        it "updates the requested trek" do
+          trek = Trek.create! valid_attributes
+          # Assuming there are no other treks in the database, this
+          # specifies that the Trek created on the previous line
+          # receives the :update_attributes message with whatever params are
+          # submitted in the request.
+          Trek.any_instance.should_receive(:update).with({ "name" => "MyString" })
+          put :update, {id: trek.to_param, trek: { "name" => "MyString" }}
+        end
+
+        it "assigns the requested trek as @trek" do
+          trek = Trek.create! valid_attributes
+          put :update, {id: trek.to_param, trek: valid_attributes}
+          assigns(:trek).should eq(trek)
+        end
+
+        it "redirects to the trek" do
+          trek = Trek.create! valid_attributes
+          put :update, {id: trek.to_param, trek: valid_attributes}
+          response.should redirect_to(trek)
+        end
+      end
+
+      describe "with invalid params" do
+        it "assigns the trek as @trek" do
+          trek = Trek.create! valid_attributes
+          # Trigger the behavior that occurs when invalid params are submitted
+          Trek.any_instance.stub(:save).and_return(false)
+          put :update, {id: trek.to_param, trek: { "name" => "" }}
+          assigns(:trek).should eq(trek)
+        end
+
+        it "re-renders the 'edit' template" do
+          trek = Trek.create! valid_attributes
+          # Trigger the behavior that occurs when invalid params are submitted
+          Trek.any_instance.stub(:save).and_return(false)
+          put :update, {id: trek.to_param, trek: { "name" => "" }}
+          response.should render_template("edit")
+        end
       end
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested trek" do
-      trek = Trek.create! valid_attributes
-      expect {
+    context 'without singing in' do
+      it "redirects to sign in page" do
+        trek = Trek.create! valid_attributes
         delete :destroy, {id: trek.to_param}
-      }.to change(Trek, :count).by(-1)
+        response.should redirect_to new_user_session_path
+      end
     end
 
-    it "redirects to the treks list" do
-      trek = Trek.create! valid_attributes
-      delete :destroy, {id: trek.to_param}
-      response.should redirect_to(treks_url)
+    context 'with singing in' do
+      before :each do
+        user_sign_in
+      end
+
+      it "destroys the requested trek" do
+        trek = Trek.create! valid_attributes
+        expect {
+          delete :destroy, {id: trek.to_param}
+        }.to change(Trek, :count).by(-1)
+      end
+
+      it "redirects to the treks list" do
+        trek = Trek.create! valid_attributes
+        delete :destroy, {id: trek.to_param}
+        response.should redirect_to(treks_url)
+      end
     end
   end
 end
--- spec/views/treks/index.html.haml_spec.rb
+++ spec/views/treks/index.html.haml_spec.rb
@@ -4,17 +4,17 @@ describe "treks/index" do
   before(:each) do
     assign(:treks, [
       stub_model(Trek,
-        :name => "Name"
+        name: "Name"
       ),
       stub_model(Trek,
-        :name => "Name"
+        name: "Another"
       )
     ])
   end
 
   it "renders a list of treks" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    rendered.should have_content 'Name'
+    rendered.should have_content 'Another'
   end
 end
