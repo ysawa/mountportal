d33fe0bfc37791cad83b9d926e7a26b99c13803d
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-07-16 00:39:21 +0900

    fix: AboutController#privacy

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe AboutController do
 
-  describe "GET 'privacy'" do
-    it "returns http success" do
-      get 'privacy'
-      response.should be_success
-    end
-  end
-
   describe "GET 'tos'" do
     it "returns http success" do
       get 'tos'
